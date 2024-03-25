e74f4b0e91536ea103088e031ed374eca00c6961
davidsan <david.sanluis@qrsof.com>
2024-03-25 11:12:44 -0600

    buttin deep links

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -5,10 +5,18 @@ export function Counter () {
 
     return [
         <>
-            <button onClick={() => incrementCounter(counter => counter + 1)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                Incrementar <span className="text-xs">{counter}</span>
-            </button>
-        
+            {/*<button onClick={() => incrementCounter(counter => counter + 1)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">*/}
+            {/*    Incrementar <span className="text-xs">{counter}</span>*/}
+            {/*</button>*/}
+            <a href="http://main--aesthetic-sorbet-c6fde1.netlify.app/?userKey=2w3w23&token=asdwadwadwa2e3"
+               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                deeplink parameter
+            </a>
+            <a href="https://main--aesthetic-sorbet-c6fde1.netlify.app"
+               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                deeplink no parameter
+            </a>
+
         </>
     ]
 }
